04774c9d84655f4b724c8908018156a8d5fcdb9c
Nikolas Göbel <niko@clockworks.io>
2015-10-21 15:49:59 +0200

    [ADD] ClassDecl

--- HW2/src/cd/frontend/parser/JavaliAstVisitor.java
+++ HW2/src/cd/frontend/parser/JavaliAstVisitor.java
@@ -1,22 +1,34 @@
 package cd.frontend.parser;
 
-import cd.ToDoException;
+import cd.ir.Ast;
 import cd.ir.Ast.ClassDecl;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import cd.frontend.parser.JavaliParser.ClassDeclContext;
-															// should be Ast, not Void
-public final class JavaliAstVisitor extends JavaliBaseVisitor<Void> {
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public final class JavaliAstVisitor extends JavaliBaseVisitor<Ast> {
 	
 	public List<ClassDecl> classDecls = new ArrayList<>();
 
 	@Override
-	public Void visitClassDecl(ClassDeclContext ctx) {
-		{
-			// classDecls = ...;
-			throw new ToDoException();
-		}
-	}
+	public Ast visitClassDecl(ClassDeclContext ctx) {
+        String className = ctx.getChild(1).getText();
+        String superName = null;
+        if (ctx.getChildCount() > 5) {
+            superName = ctx.getChild(5).getText();
+        }
+
+        List<Ast> members = new ArrayList<Ast>();
+        for (ParseTree member : ctx.memberList().children) {
+            members.add(visit(member));
+        }
+
+        ClassDecl cls = new ClassDecl(className, superName, members);
+        classDecls.add(cls);
+
+        return cls;
+    }
 }
