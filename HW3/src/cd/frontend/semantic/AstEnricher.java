bc15a045a051c43b9f988bcaf745b2a2cfdb6f35
Nikolas Göbel <niko@clockworks.io>
2015-11-06 00:46:03 +0100

    [FIX] Type error in AstEnricher.

--- HW3/src/cd/frontend/semantic/AstEnricher.java
+++ HW3/src/cd/frontend/semantic/AstEnricher.java
@@ -1,13 +1,17 @@
 package cd.frontend.semantic;
 
+import cd.ir.Ast;
 import cd.ir.AstVisitor;
+import cd.ir.Symbol;
 
 /**
  *
  */
-public class AstEnricher<R,A> extends AstVisitor<R,A>{
+public class AstEnricher extends AstVisitor<Symbol,Void> {
 
-
-
+    @Override
+    public Symbol classDecl(Ast.ClassDecl ast, A arg) {
+        return new Symbol.ClassSymbol(ast);
+    }
 
 }
