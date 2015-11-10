515710d4515a2061cc488d47661540adf59bd6af
a37 <a.fasciati@gmail.com>
2015-11-10 15:49:27 +0100

    [ADD] No_Such_Type and Circular_Inheritance checking.

--- HW3/src/cd/frontend/semantic/AstSemanticChecker.java
+++ HW3/src/cd/frontend/semantic/AstSemanticChecker.java
@@ -13,12 +13,26 @@ public class AstSemanticChecker extends AstVisitor<Void, Map<String,Symbol.Class
 
     @Override
     public Void classDecl(Ast.ClassDecl ast, Map<String,Symbol.ClassSymbol> globalSymbolTable) {
+
+        // NO_SUCH_TYPE
+        //
         // only class types can be extended from
-        if (!(globalSymbolTable.containsValue(ast.superClass))) {
+        if (ast.superClass == "int" || ast.superClass == "boolean") {
+            String errorFmt = "Class %s extends primitive type.";
+            throw new SemanticFailure(
+                    SemanticFailure.Cause.NO_SUCH_TYPE, errorFmt, ast.name);
+        // extended types have to exist
+        } else if (!(globalSymbolTable.containsValue(ast.superClass))) {
             String errorFmt = "Class %s extends non-existent type.";
             throw new SemanticFailure(
-                    SemanticFailure.Cause.NO_SUCH_TYPE, errorFmt, ast.sym.name);
+                    SemanticFailure.Cause.NO_SUCH_TYPE, errorFmt, ast.name);
+        }
 
+        // CIRCULAR_INHERITANCE
+        if (circularInheritanceChecker(ast, ast.name)){
+            String errorFmt = "Class %s should not extend %s.";
+            throw new SemanticFailure(
+                    SemanticFailure.Cause.CIRCULAR_INHERITANCE, errorFmt, ast.name, ast.superClass);
         }
 
         return null;
@@ -31,6 +45,23 @@ public class AstSemanticChecker extends AstVisitor<Void, Map<String,Symbol.Class
         }
 
         return null;
+    }
+
+
+    // Utility methods.
+
+    /**
+     *  Goes through the inheritance tree and checks if the className
+     *  occurs somewhere, thus detecting circular inheritance.
+     */
+    private Boolean circularInheritanceChecker(Ast.ClassDecl ast, String className) {
+        if (ast.superClass == "Object"){
+            return false;
+        } else if (ast.superClass == className) {
+            return true;
+        } else {
+            return circularInheritanceChecker(ast.sym.superClass.ast, className);
+        }
     }
 
 }
