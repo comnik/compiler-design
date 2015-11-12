3b9878db55a0b2087f92f39f417fa2f4445a1e1f
Nikolas Göbel <niko@clockworks.io>
2015-11-12 03:41:43 +0100

    [FIX] Empty return statements.

--- HW3/src/cd/frontend/semantic/AstTypeChecker.java
+++ HW3/src/cd/frontend/semantic/AstTypeChecker.java
@@ -295,14 +295,18 @@ public class AstTypeChecker extends AstVisitor<Symbol.TypeSymbol,Symbol> {
 
     @Override
     public Symbol.TypeSymbol returnStmt(Ast.ReturnStmt ast, Symbol parent) {
-        Symbol.TypeSymbol returnType = visit(ast.arg(), parent);
+        if (ast.arg() == null) {
+            return PrimitiveTypeSymbol.voidType;
+        } else {
+            Symbol.TypeSymbol returnType = visit(ast.arg(), parent);
 
-        if (!returnType.isSubtype(((Symbol.MethodSymbol) parent).returnType)) {
-            throw new SemanticFailure(SemanticFailure.Cause.TYPE_ERROR,
-                    "Returned value does not match expected type.");
+            if (!returnType.isSubtype(((Symbol.MethodSymbol) parent).returnType)) {
+                throw new SemanticFailure(SemanticFailure.Cause.TYPE_ERROR,
+                        "Returned value does not match expected type.");
+            }
+
+            return returnType;
         }
-
-        return returnType;
     }
 
     @Override
