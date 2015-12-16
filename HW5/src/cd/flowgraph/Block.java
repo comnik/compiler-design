dd4e41e70490ea47c7066237b38ae141f25e2d10
niko <me@nikolasgoebel.com>
2015-12-16 16:27:28 +0100

    [ADD] Block breaks.

--- HW5/src/cd/flowgraph/Block.java
+++ HW5/src/cd/flowgraph/Block.java
@@ -1,37 +1,48 @@
 package cd.flowgraph;
 
+import cd.ir.Ast.Expr;
+
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
- *
+ * Represents a basic block of execution.
  */
-public class Block<Type> {
+public class Block {
 
-    public Optional<Block> predecessor;
-    public Optional<Block> successor1;
-    public Optional<Block> successor2;
+    private final Set<Block> predecessors = new HashSet<>();
+    private final Set<Block> successors = new HashSet<>();
 
-    public Set<Type> gen;
-    public Set<Type> kill;
+    // All expressions generated inside this block.
+    private final Set<Expr> gen = new HashSet<>();
+
+    // All expressions invalidated inside this block.
+    private final Set<Expr> kill = new HashSet<>();
+
+    public Block(Block... predecessors) {
+        Stream.of(predecessors).forEach(pred -> Block.link(pred, this));
+    }
+
+    public static void link(Block pred, Block succ) {
+        pred.successors.add(succ);
+        succ.predecessors.add(pred);
+    }
 
     // TODO
-    public Set<Type> input() {
-        Set<Type> union = new HashSet<Type>(gen);
+    public Set<Expr> input() {
+        Set<Expr> union = new HashSet<>(gen);
         union.addAll(gen);
         return union;
     }
 
-    public Set<Type> out() {
+    /** Returns the set of all expressions that are valid after this block executes. */
+    public Set<Expr> out() {
         // Union of in and gen.
-        Set<Type> union = new HashSet<Type>(input());
+        Set<Expr> union = new HashSet<>(input());
         union.addAll(gen);
         // Remove kill.
         union.removeAll(kill);
         return union;
     }
 
-    public Block(Set<Type> gen, Set<Type> kill) {
-        this.gen = gen;
-        this.kill = kill;
-    }
 }
--- HW5/src/cd/flowgraph/GraphCreatorVisitor.java
+++ HW5/src/cd/flowgraph/GraphCreatorVisitor.java
@@ -1,7 +1,57 @@
 package cd.flowgraph;
 
+import cd.ir.Ast;
+import cd.ir.AstVisitor;
+
+import java.util.Optional;
+
 /**
- *
+ * Goes through the AST and creates the flow-graph.
+ * Returns the START block. The current block is passed as the second argument.
  */
-public class GraphCreatorVisitor {
+public class GraphCreatorVisitor extends AstVisitor<Block,Block> {
+
+    /** Wraps the visit function to provide a more meaningful entry-point. */
+    public Block createGraph(Ast ast) {
+        Block startBlock = new Block();
+
+        // This returns the exit block.
+        visit(ast, startBlock);
+
+        return startBlock;
+    }
+
+    @Override
+    public Block methodDecl(Ast.MethodDecl ast, Block block) {
+        return visit(ast.body(), block);
+    }
+
+    @Override
+    public Block ifElse(Ast.IfElse ast, Block prev) {
+        // The current block, prev, ends here.
+        Block ifElseBlock = new Block();
+
+        if (ast.then() != null) {
+            Block.link(visit(ast.then(), new Block(prev)), ifElseBlock);
+        }
+
+        if (ast.otherwise() != null) {
+            Block.link(visit(ast.otherwise(), new Block(prev)), ifElseBlock);
+        }
+
+        return ifElseBlock;
+    }
+
+    @Override
+    public Block whileLoop(Ast.WhileLoop ast, Block prev) {
+        // The current block, prev, ends here.
+        Block whileBlock = new Block();
+
+        if (ast.body() != null) {
+            Block.link(visit(ast.body(), new Block(prev)), whileBlock);
+        }
+
+        return whileBlock;
+    }
+
 }
